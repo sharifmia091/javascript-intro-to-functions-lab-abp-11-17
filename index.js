636ee208ffc0de2c899cd0ba92e4892dd2e66668
md sharif mia <sharifmia091@gmail.com>
2017-11-17 05:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function sayHiToGrandma(string){
   if (string.toLowerCase()== string){
     return "I can\'t hear you!"
   }
+
+  else if (string.toUpperCase()== string) {
+    return "YES INDEED!"
+      }
 }
 
-function sayHiToGrandma(string){
-  if (string.toUpperCase()== string){
-    return "YES INDEED!"
-  }
-}
+
